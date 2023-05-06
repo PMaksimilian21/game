9ae01341efaad64e8feccc4cf19bcf067ec812db
PMaksimilian21 <132762312+PMaksimilian21@users.noreply.github.com>
2023-05-06 17:07:27 +0300

    Add files via upload

---
+++ catch_up.py
@@ -0,0 +1,53 @@
+from pygame import *
+
+window = display.set_mode((700, 500))
+display.set_caption("Догонялки")
+background = transform.scale(image.load("background.png"),(700,500))
+clock = time.Clock()
+
+x1 = 100
+y1 = 300
+
+x2= 300
+y2= 300
+
+sprite1 = transform.scale(image.load('sprite1.png'),(50, 50))
+sprite2 = transform.scale(image.load('sprite2.png'),(50, 50))
+speed = 1
+
+run = True
+while run:
+    window.blit(background, (0,0))
+    window.blit(sprite1, (x1,y1))
+    window.blit(sprite2, (x2,y2))
+
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+
+    keys_pressed = key.get_pressed()
+
+    if keys_pressed[K_LEFT] and x2>5:
+        x2 -=speed
+    if keys_pressed[K_RIGHT] and x2<595:
+        x2 +=speed
+    if keys_pressed[K_UP] and y2>5:
+        y2 -=speed
+    if keys_pressed[K_DOWN] and y2<394:
+        y2 +=speed
+    
+
+
+    if keys_pressed[K_a] and x1>5:
+        x1 -=speed
+    if keys_pressed[K_d] and x1<595:
+        x1 +=speed
+    if keys_pressed[K_w] and y1>5:
+        y1 -=speed
+    if keys_pressed[K_s] and y1<394:
+        y1 +=speed
+
+    display.update()
+
+
+clock.tick(60 )
